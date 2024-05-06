91cf18b88be2a93a6405c512d071b6624a5df525
Raul <raulelwork@gmail.com>
2024-05-06 12:17:50 +0200

    Init

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Http\Controllers\Controller;
+
+
+class ProductController extends Controller
+{
+    //
+}
